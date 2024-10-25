75df7a27a544926a74fc8d71ac02bbfc194de789
KingJ89 <janmutewera@gmail.com>
2024-10-25 19:54:58 +0000

    Task 8 all completed sccessfully

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that lists all documents"""
+
+def list_all(mongo_collection);
+"""
+this is an empty list of docs in collection
+"""
+return mono_collection.find()
